7fc39102ec8d80d0aea1dbf4a6b5c4d40717e961
María Fernanda González <mafergonzalezw@gmail.com>
2024-09-09 17:52:39 +0000

    Finalizado

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+# create the jackson family object*
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
@@ -25,18 +25,49 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
 
-    # this is how you can use the Family datastructure by calling its methods
+
+@app.route('/members', methods=['GET'])
+def get_members():
     members = jackson_family.get_all_members()
     response_body = {
         "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+@app.route('/add_members', methods=['POST'])
+def new_member():
+    body = request.json
+    if body.get("first_name", False) == False:
+        return "First name is required", 400
+    
+    if body.get("age", False) == False:
+        return "Age is required", 400
+    
+    if body.get("lucky_numbers", False) == False:
+        return "Lucky numbers are required", 400
+    
+    new_person = jackson_family.add_member(body)
+    return jsonify(new_person), 200
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    delete = jackson_family.delete_member(member_id)
+    if delete == None:
+        return "Member not found", 404
+    return delete, 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_one_member(member_id):
+
+    search_member = jackson_family.get_member(member_id)
+
+    if search_member == None:
+        return "Member not found",404
+    return search_member, 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
